326e11a780914eb7d331040f9acd6f0ca004a54a
Dean Barrow <deanjordanbarrow@gmail.com>
2025-03-03 12:56:35 +1300

    Added comments to some areas of the code in Nav3, adding dropdown menu text and testing

--- src/components/Nav3/Nav3.jsx
+++ src/components/Nav3/Nav3.jsx
@@ -1,3 +1,4 @@
+// Imports
 import React, { useState, useEffect } from "react";
 import styles from "./Nav3.module.css";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
@@ -5,13 +6,15 @@ import ExpandLessIcon from "@mui/icons-material/ExpandLess";
 import SearchIcon from "@mui/icons-material/Search";
 
 function Nav3() {
+      // Variables
       const [openDropdown, setOpenDropdown] = useState(null);
       const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
 
+      // Toggle dropdown menu
       const handleToggle = (menu) => {
             setOpenDropdown(openDropdown === menu ? null : menu);
       };
-
+      // updates isMobile state for window resize (1024px)
       useEffect(() => {
             const handleResize = () => {
                   setIsMobile(window.innerWidth <= 1024);
@@ -21,6 +24,7 @@ function Nav3() {
       }, []);
 
       return (
+            // Input + button rendered when scrn size = 1024px
             <div className={styles.navContainer}>
                   {isMobile ? (
                         <div className={styles.searchContainer}>
@@ -31,21 +35,23 @@ function Nav3() {
                         <>
                               <div className={styles.navTextLeft}>
                                     {["Find a Car", "How to Buy", "Sell your Car", "Finance & Insurance"].map(
+                                          // Mapping through an array
                                           (item, index) => (
                                                 <div key={index} className={styles.navItem}>
                                                       <h1 onClick={() => handleToggle(item)}>
-                                                            {item}{" "}
                                                             {item === "Find a Car" && (
                                                                   <SearchIcon
-                                                                        sx={{ fontSize: 18, marginLeft: "18px" }}
+                                                                        sx={{ fontSize: 18, marginLeft: "0px" }}
                                                                   />
                                                             )}
+                                                            {item} {/*SearchIcon will render before items in array */}
                                                             {openDropdown === item ? (
                                                                   <ExpandLessIcon sx={{ fontSize: 12 }} />
                                                             ) : (
                                                                   <ExpandMoreIcon sx={{ fontSize: 12 }} />
                                                             )}
                                                       </h1>
+                                                      {/* dropdown menu for items in array */}
                                                       {openDropdown === item && (
                                                             <div className={styles.dropdownMenu}>
                                                                   <h2>Dropdown content for {item}</h2>
@@ -57,18 +63,23 @@ function Nav3() {
                               </div>
 
                               <div className={styles.navTextRight}>
+                                    {/* Mapping through array of items for right side nav */}
                                     {["Auctions", "Service & Info"].map((item, index) => (
                                           <div key={index} className={styles.navItem}>
                                                 <h1 onClick={() => handleToggle(item)}>
-                                                      {item}
+                                                      {item} {/* MUI icons will render AFTER items */}
                                                       {openDropdown === item ? (
                                                             <ExpandLessIcon sx={{ fontSize: 12 }} />
                                                       ) : (
                                                             <ExpandMoreIcon sx={{ fontSize: 12 }} />
                                                       )}
                                                 </h1>
+                                                {/* dropdown menu for items in array */}
                                                 {openDropdown === item && (
                                                       <div className={styles.dropdownMenu}>
+                                                            <h2>Dropdown content for {item}</h2>
+                                                            <h2>Dropdown content for {item}</h2>
+                                                            <h2>Dropdown content for {item}</h2>
                                                             <h2>Dropdown content for {item}</h2>
                                                       </div>
                                                 )}
