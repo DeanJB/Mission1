243d50ecee001d724c185fb1e495a656ca65b29c
Dean Barrow <deanjordanbarrow@gmail.com>
2025-03-02 23:04:42 +1300

    Doing my best to replicate simliar layout to turners cars website, useState applied to handle screensize @ 1024px. Nav3 menu hidden when screensize reaches 1024px and is replaced with input box and search button. Input box and search button have been styled in CSS, have a small issue with the centering of the iputbox / button but I believe that can be correct with another media query

--- src/components/Nav3/Nav3.jsx
+++ src/components/Nav3/Nav3.jsx
@@ -6,55 +6,77 @@ import SearchIcon from "@mui/icons-material/Search";
 
 function Nav3() {
       const [openDropdown, setOpenDropdown] = useState(null);
+      const [isMobile, setIsMobile] = useState(window.innerWidth <= 1024);
 
       const handleToggle = (menu) => {
             setOpenDropdown(openDropdown === menu ? null : menu);
       };
 
+      useEffect(() => {
+            const handleResize = () => {
+                  setIsMobile(window.innerWidth <= 1024);
+            };
+            window.addEventListener("resize", handleResize);
+            return () => window.removeEventListener("resize", handleResize);
+      }, []);
+
       return (
             <div className={styles.navContainer}>
-                  <div className={styles.navTextLeft}>
-                        {["Find a Car", "How to Buy", "Sell your Car", "Finance & Insurance"].map((item, index) => (
-                              <div key={index} className={styles.navItem}>
-                                    <h1 onClick={() => handleToggle(item)}>
-                                          {item}{" "}
-                                          {item === "Find a Car" && (
-                                                <SearchIcon sx={{ fontSize: 18, marginLeft: "18px" }} />
-                                          )}
-                                          {openDropdown === item ? (
-                                                <ExpandLessIcon sx={{ fontSize: 12 }} />
-                                          ) : (
-                                                <ExpandMoreIcon sx={{ fontSize: 12 }} />
-                                          )}
-                                    </h1>
-                                    {openDropdown === item && (
-                                          <div className={styles.dropdownMenu}>
-                                                <h2>Dropdown content for {item}</h2>
-                                          </div>
+                  {isMobile ? (
+                        <div className={styles.searchContainer}>
+                              <input type="text" placeholder="type here" className={styles.searchInput} />
+                              <button className={styles.searchButton}>SEARCH</button>
+                        </div>
+                  ) : (
+                        <>
+                              <div className={styles.navTextLeft}>
+                                    {["Find a Car", "How to Buy", "Sell your Car", "Finance & Insurance"].map(
+                                          (item, index) => (
+                                                <div key={index} className={styles.navItem}>
+                                                      <h1 onClick={() => handleToggle(item)}>
+                                                            {item}{" "}
+                                                            {item === "Find a Car" && (
+                                                                  <SearchIcon
+                                                                        sx={{ fontSize: 18, marginLeft: "18px" }}
+                                                                  />
+                                                            )}
+                                                            {openDropdown === item ? (
+                                                                  <ExpandLessIcon sx={{ fontSize: 12 }} />
+                                                            ) : (
+                                                                  <ExpandMoreIcon sx={{ fontSize: 12 }} />
+                                                            )}
+                                                      </h1>
+                                                      {openDropdown === item && (
+                                                            <div className={styles.dropdownMenu}>
+                                                                  <h2>Dropdown content for {item}</h2>
+                                                            </div>
+                                                      )}
+                                                </div>
+                                          )
                                     )}
                               </div>
-                        ))}
-                  </div>
 
-                  <div className={styles.navTextRight}>
-                        {["Auctions", "Service & Info"].map((item, index) => (
-                              <div key={index} className={styles.navItem}>
-                                    <h1 onClick={() => handleToggle(item)}>
-                                          {item}
-                                          {openDropdown === item ? (
-                                                <ExpandLessIcon sx={{ fontSize: 12 }} />
-                                          ) : (
-                                                <ExpandMoreIcon sx={{ fontSize: 12 }} />
-                                          )}
-                                    </h1>
-                                    {openDropdown === item && (
-                                          <div className={styles.dropdownMenu}>
-                                                <h2>Dropdown content for {item}</h2>
+                              <div className={styles.navTextRight}>
+                                    {["Auctions", "Service & Info"].map((item, index) => (
+                                          <div key={index} className={styles.navItem}>
+                                                <h1 onClick={() => handleToggle(item)}>
+                                                      {item}
+                                                      {openDropdown === item ? (
+                                                            <ExpandLessIcon sx={{ fontSize: 12 }} />
+                                                      ) : (
+                                                            <ExpandMoreIcon sx={{ fontSize: 12 }} />
+                                                      )}
+                                                </h1>
+                                                {openDropdown === item && (
+                                                      <div className={styles.dropdownMenu}>
+                                                            <h2>Dropdown content for {item}</h2>
+                                                      </div>
+                                                )}
                                           </div>
-                                    )}
+                                    ))}
                               </div>
-                        ))}
-                  </div>
+                        </>
+                  )}
             </div>
       );
 }
