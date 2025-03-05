69b16674a02c06895ce7c5f982a06ac1fa42cbf3
Dean Barrow <deanjordanbarrow@gmail.com>
2025-03-05 15:51:44 +1300

    Renamed CSS file, setup the boiler plate in CustomVisionAI.jsx

---
+++ src/components/CustomVisionAI/CustomVisiionAI.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./CustomVisionAI/CustomVisiionAI";
+
+function CustomVisiionAI() {
+      return <div className={styles.container}>CustomVisiionAI</div>;
+}
+
+export default CustomVisiionAI;
