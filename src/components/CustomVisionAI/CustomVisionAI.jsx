7a39fda2c1a5f3ea04ef16536db206d5dc7e50d2
Dean Barrow <deanjordanbarrow@gmail.com>
2025-03-05 23:44:59 +1300

    Had some syntax errors in customvisionAI.jsx, incorrect variables etc. Will work on backend axios

--- src/components/CustomVisionAI/CustomVisionAI.jsx
+++ src/components/CustomVisionAI/CustomVisionAI.jsx
@@ -5,20 +5,55 @@ import axios from "axios";
 
 function CustomVisionAI() {
       // Variables
-      const [file, setFile] = useState(null);
+      const [imageFile, setImageFile] = useState(null);
       const [prediction, setPrediction] = useState();
 
       const handleFileChange = (e) => {
-            setFile(e.target.files[0]);
+            setImageFile(e.target.files[0]);
       };
+
+      //API
+      const handleUpload = async () => {
+            if (!imageFile) return alert("Please select an image");
+
+            const formData = new FormData();
+            formData.append("image", imageFile);
+
+            try {
+                  const response = await axios.post(
+                        "https://customvisionaitest-prediction.cognitiveservices.azure.com/",
+                        formData,
+                        {
+                              headers: { "Content-Type": "multipart/form-data" },
+                        }
+                  );
+
+                  setPrediction(response.data.prediction);
+            } catch (error) {
+                  console.error("Crystal ball says error:", error);
+            }
+      };
+
       return (
             <div className={styles.container}>
                   <div>
                         <label>
-                              Upload Image
-                              <input type="file" name="myImage" accept="image/png, image/gif, image/jpeg" />
+                              <h1>Upload Image</h1>
+                              <input
+                                    type="file"
+                                    name="myImage"
+                                    accept="image/png, image/gif, image/jpeg"
+                                    onChange={handleFileChange}
+                              />
                         </label>
-                        <button onClick={uploadImage}>Analyze</button>
+                        <button onClick={handleUpload}>Analyze</button>
+
+                        {prediction && (
+                              <div>
+                                    <h3>Result:</h3>
+                                    <p>{prediction}</p>
+                              </div>
+                        )}
                   </div>
             </div>
       );
